1e08f0093dd42139dfd9f4314efc282298c19c22
chamberrr2809 <chamberlynsiagian123@candletreeschool.sch.id>
2022-01-05 22:18:59 +0700

    update

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDi34cep-XVkqe2SWt8Os2eYDs3tIbqEJE',
-  authDomain: 'mychat-backend.firebaseapp.com',
-  projectId: 'mychat-backend',
-  storageBucket: 'mychat-backend.appspot.com',
-  messagingSenderId: '424137266241',
-  appId: '1:424137266241:web:995c82ac93c2806696e2c0',
-  measurementId: 'G-LBKT8TNQ9R',
+  // TO-DO: Place Your Firebase Config here
 };
 
 const app = initializeApp(firebaseConfig);
